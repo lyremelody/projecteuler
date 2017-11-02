fb738eb53546c501800e94a0d612cbca3304bfb1
lyremelody <lyremelody@163.com>
2017-11-02 15:47:41 +0800

    2nd solution for problem 2

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+# solution 1
 def sum_of_fibonacci_even(max_limit):
     a = 1
     b = 2
@@ -20,7 +21,31 @@ def sum_of_fibonacci_even(max_limit):
  
     return sum_of_even
 
+
+# solution 2
+def fibonacci_sequence(max_limit):
+    fib_seq = list([1, 2])
+    while True:
+        tmp = fib_seq[-1] + fib_seq[-2]
+        if tmp > max_limit:
+            break
+        fib_seq.append(tmp)
+    return fib_seq
+
+
+def sum_of_fibonacci_even2(max_limit):
+    sum_of_even = 0
+    for el in fibonacci_sequence(max_limit):
+        if el % 2 == 0:
+            sum_of_even += el
+
+    return sum_of_even
+
+
 if __name__ == '__main__':
-    result = sum_of_fibonacci_even(4000000)
+    #result = sum_of_fibonacci_even(4000000)
+    #print 'result: ', result
+
+    result = sum_of_fibonacci_even2(4000000)
     print 'result: ', result
         
